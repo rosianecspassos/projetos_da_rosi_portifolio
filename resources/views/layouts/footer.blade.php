d80e6bfbc9a6c0b210533981521df5ba150be3a7
Rosiane C S dos Passos <rosianecristinasi2013@gmail.com>
2025-06-20 14:12:31 -0300

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,8 @@
-<div class= "container text-light"> 
-<div class="row mt-5">
-    <div class="col-md-4">
-<p class="mt-5">&copy; 2025 Rosiane Cristina. Todos os direitos </p> 
-
+    <div class="container d-flex flex-column flex-md-row justify-content-between">
+      <p>&copy; 2025 Rosiane Cristina. Todos os direitos reservados.</p>
+      <ul class="d-flex list-unstyled justify-content-center gap-3">
+        <li><a href="https://github.com/rosianecspassos"><i class="fa fa-github"></i></a></li>
+        <li><a href="https://api.whatsapp.com/send?phone=5534996619291"><i class="fa fa-whatsapp"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/rosiane-cristina-souza-dos-passos-38390645/"><i class="fa fa-linkedin"></i></a></li>
+      </ul>
     </div>
-    <div class="col-md-2 ms-auto">
-    <ul class="mt-5 text-center">  
-      <li><a href="https://github.com/rosianecspassos"><i class="fa fa-github" style="font-size:24px"></i></a></li>
-      <li><a href="https://api.whatsapp.com/send?phone=5534996619291"><i class="fa fa-whatsapp" style="font-size:24px"></i></a></li>
-      <li><a href="https://www.linkedin.com/in/rosiane-cristina-souza-dos-passos-38390645/"><i class="fa fa-linkedin" style="font-size:24px"></i></a></li>
-</ul> 
-    </div>
-    
-  </div>
-</div> 
