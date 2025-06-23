3e1d33778e33e73eb033fe063a16971d6f610a56
Rosiane C S dos Passos <rosianecristinasi2013@gmail.com>
2025-06-23 09:59:10 -0300

    Add files via upload

--- resources/views/projetos/index.blade.php
+++ resources/views/projetos/index.blade.php
@@ -10,31 +10,27 @@
 
 <!-- Conteúdo -->
 @section('conteudo')
-<div class = "container-fluid w-100 "> 
-    <div class = "container-fluid w-50"> 
-<h2 class = "text-center text-light border border-light border-start-0 border-top-0 border-end-0 mt-3">Portifólio</h2> 
-</div> 
-<div class="row justify-content-center mt-5">
-    @foreach($projetos as $projeto)
-        <div class="col-md-3 mb-4">
-            <a href="{{ $projeto->link }}" target="_blank" style="text-decoration: none; color: inherit;">
-                <div class="card h-100">
-                    @if($projeto->imagem)
-                        <img src="{{ asset('storage/' . $projeto->imagem) }}" class="card-img-top mt-3 mb-3" alt="{{ $projeto->nome_projeto }}">
-                    @else
-                        <img src="{{ asset('images/imagem.jpg') }}" class="card-img-top mt-3 mb-3" alt="Imagem padrão">
-                    @endif
+    <h1 class="text-center mb-5">Portfólio</h1>
+    <div class="row justify-content-center">
+        @foreach($projetos as $projeto)
+            <div class="col-md-4 mb-4">
+                <a href="{{ $projeto->link }}" target="_blank" class="text-decoration-none text-dark">
+                    <div class="card h-100">
+                        @if($projeto->imagem)
+                            <img src="{{ asset('storage/' . $projeto->imagem) }}" class="card-img-top" style="height: 150px; object-fit: cover;" alt="{{ $projeto->nome_projeto }}">
+                        @else
+                            <img src="{{ asset('images/imagem.jpg') }}" class="card-img-top" style="height: 150px; object-fit: cover;" alt="Imagem padrão">
+                        @endif
 
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $projeto->nome_projeto }}</h5>
-                        <p class="card-text">{{ $projeto->descricao }}</p>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">{{ $projeto->nome_projeto }}</h5>
+                            <p class="card-text">{{ $projeto->descricao }}</p>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </div>
-    @endforeach
-</div>
-</div> 
+                </a>
+            </div>
+        @endforeach
+    </div>
 @endsection
 
 <!-- Rodapé -->
