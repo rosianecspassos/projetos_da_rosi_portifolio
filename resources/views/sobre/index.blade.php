de374390418753d26354bfa7123374a27caf093b
Rosiane C S dos Passos <rosianecristinasi2013@gmail.com>
2025-06-23 09:58:29 -0300

    Add files via upload

--- resources/views/sobre/index.blade.php
+++ resources/views/sobre/index.blade.php
@@ -1,5 +1,3 @@
-
-
 @extends('templates.sobre')
 
 @section('title', 'Projetos da Rosi')
@@ -9,78 +7,70 @@
 @endsection
 
 @section('conteudo')
-<div class="container py-5">
+    <h1 class="text-center mb-5">Sobre Mim</h1>
 
-    {{-- Seção Sobre --}}
-    <section class="bg-light rounded p-4 mb-5 shadow-sm">
-        <div class="text-center">
-            <h2 class="text-primary border-bottom border-primary pb-2 mb-4">Sobre</h2>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md-4 text-center">
+            <img src="images/rosiane.png" class="card-img-top rounded-circle mx-auto mt-3" style="width:120px; height:120px;">
+            <div class="redes_sociais">
+                <ul class="d-flex justify-content-center">
+                    <li><a href="https://github.com/rosianecspassos"><i class="fa fa-github"></i></a></li>
+                    <li><a href="https://api.whatsapp.com/send?phone=5534996619291"><i class="fa fa-whatsapp"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/rosiane-cristina-souza-dos-passos-38390645/"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
 
+        <div class="col-md-8">
             @if($sobres->isNotEmpty() && !empty($sobres->first()->desc_sobre))
-                <p class="text-dark fs-5">{{ $sobres->first()->desc_sobre }}</p>
+                <p>{{ $sobres->first()->desc_sobre }}</p>
             @else
-                <p class="text-dark fs-5">Nenhuma descrição "Sobre" disponível.</p>
+                <p>Nenhuma descrição "Sobre" disponível.</p>
             @endif
         </div>
-    </section>
+    </div>
 
-    {{-- Seção Cursos --}}
-    <section class="bg-primary text-light rounded p-4 mb-5 shadow-sm">
-        <div class="text-center mb-4">
-            <h2 class="border-bottom border-light pb-2">Cursos</h2>
-        </div>
-        <div class="row g-4">
+    <section class="mb-5">
+        <h2>Cursos</h2>
+        <div class="row g-3">
             @if($sobres->isNotEmpty() && is_array($sobres->first()->curso))
                 @foreach($sobres->first()->curso as $index => $curso)
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card h-100 bg-primary border-0 shadow">
-                            <div class="card-body d-flex flex-column">
-                                <h4 class="card-title fw-bold mb-3">{{ $curso }}</h4>
-                                <p class="card-text text-light flex-grow-1">
-                                    {{ $sobres->first()->desc_curso[$index] ?? 'Nenhuma descrição disponível.' }}
-                                </p>
-                                <div class="mt-3 text-end">
-                                    @if(!empty($sobres->first()->link_curso[$index]))
-                                    <a href="{{ $sobres->first()->link_curso[$index] }}" target="_blank" class="btn btn-outline-light btn-sm">Escola / Instituição: </a>
-                                    @else
-                                        <span class="text-light small fst-italic">Nenhum link disponível.</span>
-                                    @endif
-                                </div>
-                            </div>
+                    <div class="col-md-4">
+                        <div class="card cursos-card p-3 h-100">
+                            <h5 class="card-title">{{ $curso }}</h5>
+                            <p class="card-text">
+                                {{ $sobres->first()->desc_curso[$index] ?? 'Nenhuma descrição disponível.' }}
+                            </p>
+                            <p class="card-text">
+                                @if(!empty($sobres->first()->link_curso[$index]))
+                                    <a href="{{ $sobres->first()->link_curso[$index] }}" target="_blank" class="btn btn-outline-light btn-sm">
+                                        Escola / Instituição
+                                    </a>
+                                @else
+                                    Nenhum link disponível.
+                                @endif
+                            </p>
                         </div>
                     </div>
                 @endforeach
             @else
-                <div class="col-12 text-center">
-                    <p class="text-light fs-5">Nenhum curso disponível.</p>
-                </div>
+                <p class="text-light fs-5">Nenhum curso disponível.</p>
             @endif
         </div>
     </section>
 
-    {{-- Seção Competências --}}
-    <section class="competencias bg-light rounded p-4 shadow-sm">
-        <div class="text-center mb-4">
-            <h2 class="border-bottom border-primary pb-2">Principais Competências</h2>
-        </div>
-        <div class="row g-3 justify-content-center">
-            @if($sobres->isNotEmpty() && is_array($sobres->first()->competencia))
+    <section class="mb-5">
+        <h2>Competências</h2>
+        @if($sobres->isNotEmpty() && is_array($sobres->first()->competencia))
+            <ul>
                 @foreach($sobres->first()->competencia as $competencia)
-                    <div class="col-6 col-md-4 col-lg-3">
-                        <div class="p-3 bg-primary text-light rounded text-center shadow-sm">
-                            <h5 class="mb-0">{{ $competencia }}</h5>
-                        </div>
-                    </div>
+                    <li>{{ $competencia }}</li>
                 @endforeach
-            @else
-                <div class="col-12 text-center">
-                    <p class="fs-5">Nenhuma competência disponível.</p>
-                </div>
-            @endif
-        </div>
+            </ul>
+        @else
+            <p>Nenhuma competência disponível.</p>
+        @endif
     </section>
-
-</div>
 @endsection
 
 @section('footer')
